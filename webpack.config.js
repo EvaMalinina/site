15378794c4333a1a99fb001f4aabe8579c469c73
Tatyana Dotsenko <userdotsenko@gmail.com>
2019-03-20 19:03:28 +0700

    deleted dist from gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -91,7 +91,7 @@ module.exports = (env, argv) => {
     output: {
       path: path.resolve(__dirname, "./dist"),
       filename: "[name].[hash].build.js",
-      publicPath: isProductionBuild ? 'https://evamalinina.github.io/site/' : "",
+      publicPath: isProductionBuild ? publicPath : "",
       chunkFilename: "[chunkhash].js"
     },
     module: {
