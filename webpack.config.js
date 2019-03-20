41a86c0bf31560047bf4fb44d9ac5c781b46df22
Tatyana Dotsenko <userdotsenko@gmail.com>
2019-03-20 21:17:33 +0700

    change publicPath3

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = (env, argv) => {
   const isProductionBuild = argv.mode === "production";
-  const publicPath = '/evamalinina.github.io/site/';
+  const publicPath = 'evamalinina.github.io/site/';
 
   const pcss = {
     test: /\.(p|post|)css$/,
