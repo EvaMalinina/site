63e05bf262223be0b7f8d90c9bafcfbfeaba5070
Tatyana Dotsenko <userdotsenko@gmail.com>
2019-03-29 01:39:15 +0700

    small fixes change structure

--- src/scripts/humburger.js
+++ src/scripts/humburger.js
@@ -1,17 +1,9 @@
-// document.addEventListener('click', function(e) {
+document.querySelector('#humburger').addEventListener('click', function(event) {
+  var header = document.querySelector('#header');
+  var user = document.querySelector('.user');
 
-//   var header = document.querySelector('#header');
-//   var user = document.querySelector('.user');
+  console.log(event.target)
 
-//   console.log(event.target)
-//   header.classList.toggle('header_fullmenu');
-//   user.classList.toggle('user_fullmenu');
-// });
-
-// document.querySelector('#humburger').addEventListener('click', function(event) {
-//   var header = document.querySelector('#header');
-//   console.log(event.target)
-
-//   header.classList.toggle('header_fullmenu');
-
-// });
+  header.classList.toggle('header_fullmenu');
+  user.classList.toggle('user_fullmenu');
+});
